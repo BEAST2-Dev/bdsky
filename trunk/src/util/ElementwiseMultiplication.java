353967891501b82870f12154718bb9fc9c7f5d85
denise.kuehnert@gmail.com <denise.kuehnert@gmail.com@14c3a62d-1125-7199-f341-64c306e0e373>
2011-04-27 03:27:01 +0000

    Adapted to new (beast2-) RealParameter constructor.

---
+++ trunk/src/util/ElementwiseMultiplication.java
@@ -0,0 +1,41 @@
+package util;
+
+import beast.core.parameter.RealParameter;
+import beast.core.*;
+
+/**
+ * @author: Denise Kuehnert
+ * Date: Apr 12, 2011
+ * Time: 2:41:12 PM
+ */
+
+@Description("Multiply each element of a vector with the given scalar")
+public class ElementwiseMultiplication extends Plugin{
+
+    public Input<RealParameter> vector =
+                new Input<RealParameter>("vector", "vector to be multiplied by the scalar", Input.Validate.REQUIRED);
+
+    public Input<Double> scalar =
+                    new Input<Double>("scalar", "scalar to multiply with vector", Input.Validate.REQUIRED);
+
+
+    RealParameter multipliedVector;
+
+
+    /* Methods */
+    public void initAndValidate() throws Exception {
+        Double[] mult = new Double[vector.get().getDimension()];
+        for (int i =0; i<mult.length; i++){
+            mult[i] =   vector.get().getArrayValue(i) * scalar.get();
+        }
+
+        multipliedVector = new RealParameter(mult);
+    }
+
+    public RealParameter get() throws Exception {
+
+        return multipliedVector;
+
+    }
+
+}
