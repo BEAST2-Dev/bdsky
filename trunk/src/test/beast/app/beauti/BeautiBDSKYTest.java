7d5e571480f7e6576bda6a0449d37e32f4418936
higgs.ml@gmail.com <higgs.ml@gmail.com@14c3a62d-1125-7199-f341-64c306e0e373>
2012-09-25 23:42:19 +0000

    add rudimentary BEAUti test

---
+++ trunk/src/test/beast/app/beauti/BeautiBDSKYTest.java
@@ -0,0 +1,23 @@
+package test.beast.app.beauti;
+
+import java.io.File;
+
+import org.fest.swing.fixture.JTabbedPaneFixture;
+import org.junit.Test;
+
+public class BeautiBDSKYTest extends BeautiBase {
+
+	@Test
+	public void simpleTest() throws Exception {
+		
+		importAlignment("../beast2/examples/nexus", new File("anolis.nex"));
+		JTabbedPaneFixture f = beautiFrame.tabbedPane();
+		f.selectTab("Priors");
+		
+		warning("Change Tree prior to Birth Death Skyline");
+		beautiFrame.comboBox("TreeDistribution").selectItem("Birth Death Skyline");
+		printBeautiState(f);
+
+		makeSureXMLParses();
+	}
+}
